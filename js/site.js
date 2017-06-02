28fba35cdc3a2b4f56aac22c16fb0eb00165a38b
Guus Hoekman <guus@code4sa.org>
2017-06-02 13:05:41 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -6,7 +6,7 @@ $(function() {
 });
 
 $(function() {
-  url = window.location.href
+  url = window.location.href;
   $(".page-share.facebook").on("click", function(e) {
     e.preventDefault();
     window.open("https://www.facebook.com/sharer/sharer.php?u="+url,
