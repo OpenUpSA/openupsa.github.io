000477a397dcc3cbb673facc494ac7943a16c271
Adi Eyal <adi@openup.org.za>
2018-08-27 00:13:27 +0200

    Added test for blog vs news

--- js/site.js
+++ js/site.js
@@ -49,6 +49,8 @@ $(document).ready(function() {
   $(".trainup-course .hero h1, .trainup-course.open .content h2, .trainup-course.open .content h3, .trainup-course.open .content h4").each(function() {
     $(".toc-list").append("<li class="+$(this).get(0).nodeName+"><a href='#"+$(this).attr("id")+"'>"+$(this).text()+"</a></li>");
   });
+  console.log("Applying AB");
+  ABalytics.applyHtml();
 });
 
 $(window).scroll(function() {
@@ -93,3 +95,17 @@ $(".trainup-page .academy.article-list .btn.show-more").click(function (){
     $(".trainup-page .academy.article-list .btn.show-more").hide()
   };
 });
+
+// AB Testing of content
+ABalytics.init({
+  // Test whether it is preferable to use News or Blog for the blog menu item
+  news_vs_blog: [
+    {
+      name: 'News',
+      "menu_blog": "News"
+    },
+    {
+      name: 'Blog',
+      "menu_blog": "Blog"
+    }
+]});
