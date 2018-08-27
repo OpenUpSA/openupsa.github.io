47e1f5474a7fb2b036c19f16ca646c8c1747c079
Adi Eyal <adi@openup.org.za>
2018-08-27 07:59:19 +0200

    Added default text to news menu item to prevent flickering

--- js/site.js
+++ js/site.js
@@ -49,8 +49,22 @@ $(document).ready(function() {
   $(".trainup-course .hero h1, .trainup-course.open .content h2, .trainup-course.open .content h3, .trainup-course.open .content h4").each(function() {
     $(".toc-list").append("<li class="+$(this).get(0).nodeName+"><a href='#"+$(this).attr("id")+"'>"+$(this).text()+"</a></li>");
   });
-  console.log("Applying AB");
+
+  // AB Testing of content
+  ABalytics.init({
+    // Test whether it is preferable to use News or Blog for the blog menu item
+    news_vs_blog: [
+      {
+        name: 'ab_news',
+        "menu_blog": "News"
+      },
+      {
+        name: 'ab_blog',
+        "menu_blog": "Blog"
+      }
+  ]});
   ABalytics.applyHtml();
+   _gaq.push(['_trackPageview']);
 });
 
 $(window).scroll(function() {
@@ -96,16 +110,3 @@ $(".trainup-page .academy.article-list .btn.show-more").click(function (){
   };
 });
 
-// AB Testing of content
-ABalytics.init({
-  // Test whether it is preferable to use News or Blog for the blog menu item
-  news_vs_blog: [
-    {
-      name: 'News',
-      "menu_blog": "News"
-    },
-    {
-      name: 'Blog',
-      "menu_blog": "Blog"
-    }
-]});
